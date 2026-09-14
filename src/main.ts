4e15c49ff13c6cbd927e5cbdecbd5dc1472e5408
MAlex07 <molnar.alex@petrik.hu>
2026-09-14 10:37:18 +0200

    form és megjelenés jó

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,45 @@
-import type { ElectricRat } from "./ElectricRat"
+import { ElectricRat } from "./ElectricRat"
 
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  megjelenites()
+const form = document.querySelector<HTMLFormElement>('#patkanyform');
+const input = document.querySelector<HTMLInputElement>('#name')!;
+const kartyak = document.querySelector<HTMLDivElement>('#kartyak');
+
+const patkanyLista: ElectricRat[] = [];
+
+
+function rndSzam(min: number, max: number):number{
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+form?.addEventListener("submit", (e)=>{
+  e.preventDefault();
+try{
+  const name = input.value;
+  const atk = rndSzam(10, 20);
+  const hp = rndSzam(50, 100);
+  const patkany = new ElectricRat(name, atk, hp);
+  patkanyLista.push(patkany);
+
+  const card = document.createElement('div');
+  card.classList.add('kartya');
+
+  card.innerHTML = `
+  <h2>${patkany.getName()}</h2>
+  <p>sebzés: ${patkany.getAtk()}</p>
+  <p>életerő: ${patkany.getHp()}</p>
+  `;
+  kartyak?.appendChild(card);
+
+  input.value = '';
+  //console.log(patkanyLista)
+}
+
+catch(error){
+  if(error instanceof Error){
+    alert(error.message)
+  }
+}
+
 })
 
-function megjelenites(){
-
-}
-
-function ujPatkany(){
-  const rnd = Math.random();
-
-  const newratName = document.getElementById('name');
-  //const newAtk = 
-
-  
-
-}
